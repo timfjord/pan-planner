780f22a9b667d3fbac2b4057d51e2411179b9a91
Tima Maslyuchenko <dudapip3@gmail.com>
2012-06-22 18:31:45 +0300

    init rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PanPlanner::Application.config.secret_token = '4320a7b6dfb0878f6b22f6987367c135b14df163df50d4abf76163f0fdabb71a068e687c8e241fcc028bfc6e45c493056a8c14b6b67d41d85a71d64bc3cf6e53'
