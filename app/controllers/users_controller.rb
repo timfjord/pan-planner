9b16d5143bf1a0c3710a203897194f5ec78cedff
Tima Maslyuchenko <dudapip3@gmail.com>
2012-06-29 14:20:17 +0300

    apply bootstrap for users reousrce

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "admin", except: 'index'
+  
   # GET /users
   # GET /users.json
   def index
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe "Users" do
   describe "GET /users" do
-    it "should shows users" do
+    it "shows users" do
       visit users_path
       page.status_code.should be 200
+    end
+    
+    it "should show user create page only for admin" do
+      visit new_user_path
+      page.status_code.should be 401
     end
   end
 end
