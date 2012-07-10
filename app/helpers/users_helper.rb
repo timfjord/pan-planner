712a6413336b3c598775686ac02ac9015b348a87
Tima Maslyuchenko <dudapip3@gmail.com>
2012-07-10 15:38:49 +0300

    added target=_blank as default option for user_list helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
   def user_list(users, opt = {})
     separator = opt.delete(:separator) || ' '
+    opt.merge!(target: '_blank')
     users.map do |u| 
       opt[:href] = "https://github.com/#{u.github_username}"
       content_tag 'a', u.name, opt 
