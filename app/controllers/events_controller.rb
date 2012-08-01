36a347d973fa152b248344224580a4aee3e785ee
Tima Maslyuchenko <dudapip3@gmail.com>
2012-08-01 12:50:29 +0300

    refactored events calendar rendering after later_dude update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,9 +6,8 @@ class EventsController < ApplicationController
   def index
     date_params = params.symbolize_keys.slice(:year, :month).transform_values(&:to_i)
     @event_date = DateTime.now.change date_params
-    # refactor this in the future, to avoid duplication
-    calendar = LaterDude::Calendar.new(@event_date.year, @event_date.month, first_day_of_week: 1)
-    @events = Event.for_calendar(calendar.first_rendered_date..calendar.last_rendered_date)
+    @calendar = LaterDude::Calendar.new(@event_date.year, @event_date.month, use_full_day_names: true, first_day_of_week: 1, hide_month_name: true, yield_surrounding_days: true)
+    @events = Event.for_calendar(@calendar.first_rendered_date..@calendar.last_rendered_date)
     @next_event = Event.next_event
     index!
   end
